8b70ecdd8931c12d29b5b2564d49517fe04548de
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-11-03 22:26:28 +0300

    Верстаю главную страницу

--- Lopyshok/Pages/ProductListPage.xaml.cs
+++ Lopyshok/Pages/ProductListPage.xaml.cs
@@ -21,13 +21,50 @@ namespace Lopyshok.Pages
     /// </summary>
     public partial class ProductListPage : Page
     {
-        List<Product> products { get; set; }
+        public List<Product> products { get; set; }
+        public List<ProductType> types { get; set; }
         public ProductListPage()
         {
             InitializeComponent();
             products = bd_connection.connection.Product.ToList();
 
+            types = bd_connection.connection.ProductType.ToList();
+            types.Insert(0, new ProductType { Id = 0, Name = "Все типы" });
+            cbFiltr.ItemsSource = types;
+            cbFiltr.DisplayMemberPath = "Name";
+
             this.DataContext = this;
+        }
+
+        public void Filter()
+        {
+            List<Product> filterProduct = products;
+
+            if(tbSearch.Text.Trim().Length != 0)
+            {
+                filterProduct = filterProduct.Where(x => x.Name.Contains(tbSearch.Text.Trim())).ToList();
+            }
+
+            if(cbFiltr.SelectedItem != null)
+            {
+                var selectFiltr = cbFiltr.SelectedItem as ProductType;
+                if (selectFiltr.Id != 0)
+                {
+                    filterProduct = filterProduct.Where(x => x.ProductType.Id == selectFiltr.Id).ToList();
+                }
+            }
+
+            lvProduct.ItemsSource = filterProduct;
+        }
+
+        private void tbSearchSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbFiltrSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
         }
     }
 }
